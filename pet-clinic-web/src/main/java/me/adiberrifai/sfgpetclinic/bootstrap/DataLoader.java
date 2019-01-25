d3ffb2fc5461f51e8cffaec86cc2352ad253c581
Adib Err <adib.err@hotmail.com>
2019-01-25 01:53:32 +0000

    Added pets and Contact info to owners on startup. Closes #32.

--- pet-clinic-web/src/main/java/me/adiberrifai/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/me/adiberrifai/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package me.adiberrifai.sfgpetclinic.bootstrap;
 
 import me.adiberrifai.sfgpetclinic.model.Owner;
+import me.adiberrifai.sfgpetclinic.model.Pet;
 import me.adiberrifai.sfgpetclinic.model.PetType;
 import me.adiberrifai.sfgpetclinic.model.Vet;
 import me.adiberrifai.sfgpetclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import me.adiberrifai.sfgpetclinic.services.PetTypeService;
 import me.adiberrifai.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -38,12 +41,32 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("John");
         owner1.setLastName("Doe");
+        owner1.setAddress("123 Brickerel");
+        owner1.setCity("Miami");
+        owner1.setTelephone("123456789");
+
+        Pet johnsPet = new Pet();
+        johnsPet.setName("Rex");
+        johnsPet.setPetType(savedDogPetType);
+        johnsPet.setBirthDate(LocalDate.now());
+        johnsPet.setOwner(owner1);
+        owner1.getPets().add(johnsPet);
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Jane");
         owner2.setLastName("Doe");
+        owner2.setAddress("123 Brickerel");
+        owner2.setCity("Miami");
+        owner2.setTelephone("123456789");
+
+        Pet janePet = new Pet();
+        janePet.setName("Miau");
+        janePet.setPetType(savedCatPetType);
+        janePet.setBirthDate(LocalDate.now());
+        janePet.setOwner(owner2);
+        owner2.getPets().add(janePet);
 
         ownerService.save(owner2);
 
